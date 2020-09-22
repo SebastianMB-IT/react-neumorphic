a0cd2d2637a463eed747c4a271f669448303d58c
SebastianMB-IT <sebastian.besel@nethesis.it>
2020-09-22 18:33:53 +0200

    Complete player and ranking pages

--- src/pages/player.js
+++ src/pages/player.js
@@ -3,16 +3,28 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const Player = () => (
-  <Layout>
-    <SEO title="Player" />
-    <h1>Sebastian Marius Besel</h1>
-    <h1
-      className="points-text"
-    >
-      25 points
-    </h1>
-  </Layout>
-)
+export default class Player extends React.Component {
 
-export default Player
+  constructor (props) {
+    super(props)
+    this.state = {
+      playerName : localStorage.getItem("playerName"),
+      playerPoints : JSON.parse(localStorage.getItem("playersRanking"))
+    }
+  }
+
+  render () {
+    return (
+      <Layout>
+        <SEO title="Player" />
+        <h1>{this.state.playerName}</h1>
+        <h1
+          className="points-text"
+        >
+          {this.state.playerPoints[this.state.playerName]} points
+        </h1>
+      </Layout>
+    )
+  }
+
+}
--- src/pages/ranking.tsx
+++ src/pages/ranking.tsx
@@ -1,34 +1,59 @@
 // If you don't want to use TypeScript you can delete this file!
 import React from "react"
 
-import { PageProps } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-type DataProps = {
-  site: {
-    buildTime: string
-  }
+type DataState = {
+  ranking: object[]
 }
 
-const Ranking: React.FC<PageProps<DataProps>> = ({ data, path }) => (
-  <Layout>
-    <SEO title="Ranking" />
-    <h2>Best users ranking:</h2>
-    <div className="lyric-card">
-      <ul>
-        <li>
-          Sebastian Marius Besel
-        </li>
-        <li>
-          Francesco Filippini
-        </li>
-        <li>
-          Jader Rossi
-        </li>
-      </ul>
-    </div>
-  </Layout>
-)
+export default class Ranking extends React.Component<{}, DataState> {
 
-export default Ranking
+  constructor (props) {
+    super(props)
+    this.state = {
+      ranking: []
+    }
+  }
+
+  getRanking() {
+    let ranking: object = JSON.parse(localStorage.getItem("playersRanking")) || {}
+    let rankingArray: object[] = []
+    for (let player in ranking) {
+      rankingArray.push({
+        name: player,
+        score: ranking[player]
+      })
+    }
+    this.setState({
+      ranking: rankingArray
+    })
+  }
+
+  componentDidMount() {
+    this.getRanking()
+  }
+
+  render() {
+    const rankingList = this.state.ranking.map((player, i) => {
+      return (
+        <li key={i}>
+          {player["name"]}
+          <span className="score">
+            {player["score"]} points
+          </span>
+        </li>
+      )
+    })
+    return (
+      <Layout>
+        <SEO title="Ranking" />
+        <h2>Best players ranking:</h2>
+        <ol className="ranking-list">
+          {rankingList ? rankingList : ""}
+        </ol>
+      </Layout>
+    )
+  }
+}
